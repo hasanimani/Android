8f46c30f7a8198d412267e14514b26cf3b65bc03
Amin Zahedi <azahedi@paypal.com>
2017-09-04 18:29:52 +0800

    menu items

---
+++ Hello/app/src/main/java/ir/hitaxi/hello/DrawerNavigationListAdapter.java
@@ -0,0 +1,9 @@
+package ir.hitaxi.hello;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by general on 9/4/2017.
+ */
+public class DrawerNavigationListAdapter extends ArrayAdapter {
+}
---
+++ Hello/app/src/main/java/ir/hitaxi/hello/DrawerNavigationListView.java
@@ -0,0 +1,7 @@
+package ir.hitaxi.hello;
+
+/**
+ * Created by general on 9/4/2017.
+ */
+public class DrawerNavigationListView {
+}
